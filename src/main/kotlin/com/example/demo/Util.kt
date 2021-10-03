94e41836184701dbc534532e494cecb6171d548c
Trayani <psota.j@email.cz>
2021-10-03 11:48:35 +0200

    added unit tests for correct results and parallel processing

--- src/main/kotlin/com/example/demo/Util.kt
+++ src/main/kotlin/com/example/demo/Util.kt
@@ -1,6 +1,14 @@
 package com.example.demo
 
+import com.mifmif.common.regex.Generex
+
 inline fun <reified T : Any?> T.andPrint(): T {
     println(this)
     return this
+}
+
+inline fun Generex.forEachMatch(action: (String) -> Unit){
+    val iterator = this.iterator()
+    while (iterator.hasNext())
+        action(iterator.next())
 }
